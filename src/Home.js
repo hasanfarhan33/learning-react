13957dc9470db7de587d365dd9056a9f90c9bbdf
Farhan Hasan <hasanfarhan33@gmail.com>
2023-11-11 23:13:57 +0000

    Added reactable list

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const Home = () => {
+  // Initial list of blogs
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+
+  return (
+    <div className="Home">
+      {/* For every blog create a new div with its unique blog ID taken from the JSON */}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
